ff4e2f4dc918e74465e752b0c7e391cad8298a62
Luka Vidaković <luka@uxpassion.com>
2014-03-10 13:54:45 +0100

    On open close other dropdowns by default but there's now a setting for that.
    Also there is now setting by which you can disable closing of dropdown on the same action it was opened, so it can only be removed by outside click.
    keepOpen=false only closes the dropdowns instantiated in the same batch/group
    There is now simplest possible example page.

--- dropit.js
+++ dropit.js
@@ -15,6 +15,7 @@
 
             init : function(options) {
                 this.dropit.settings = $.extend({}, this.dropit.defaults, options);
+                var dropdowns = this;
                 return this.each(function() {
                     var $el = $(this),
                          el = this,
@@ -27,11 +28,18 @@
                     
                     // Open on click
                     $el.on(settings.action, settings.triggerParentEl +':has('+ settings.submenuEl +') > '+ settings.triggerEl +'', function(){
-                        if($(this).parents(settings.triggerParentEl).hasClass('dropit-open')) {
+                        // Close if already opened
+                        if($(this).parents(settings.triggerParentEl).hasClass('dropit-open') && settings.closeOnAction) {
                             settings.beforeHide.call(this);
-                            $('.dropit-open').removeClass('dropit-open');
-                            settings.afterHide.call(this);                           
+                            $el.find('.dropit-open').removeClass('dropit-open');
+                            settings.afterHide.call(this);
                             return false;
+                        }
+                        // Close all other oppened dropdowns if settings.keepOpen is false
+                        if(settings.keepOpen === false) {
+                            dropdowns.each(function() {
+                                $(this).find('.dropit-trigger').removeClass('dropit-open');
+                            });
                         }
                         settings.beforeShow.call(this);
                         $(this).parents(settings.triggerParentEl).addClass('dropit-open');
@@ -63,7 +71,9 @@
     }
 
     $.fn.dropit.defaults = {
+        keepOpen: false, // Allow multiple opened dropdowns at the same time
         action: 'click', // The open action for the trigger
+        closeOnAction: false, // Whether to close dropdown on defined action event if it's opened
         submenuEl: 'ul', // The submenu element
         triggerEl: 'a', // The trigger element
         triggerParentEl: 'li', // The trigger parent element
