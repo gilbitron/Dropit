4d2394d4aad0069c214fb4d2f5cd519b1e724b32
Gilbert Pellegrom <gilbert@dev7studios.com>
2014-05-08 15:58:53 +0100

    v1.1.0
     * [New] Added bower.json
     * [Fixed] Page jumping issue for click menus
     * [Fixed] Close an open menu if parent element is clicked
     * [Fixed] Close an open hover menu if mouseleave
     * [Fixed] jQuery v2.x compatibility

--- dropit.js
+++ dropit.js
@@ -1,5 +1,5 @@
 /*
- * Dropit v1.0
+ * Dropit v1.1.0
  * http://dev7studios.com/dropit
  *
  * Copyright 2012, Dev7studios
@@ -19,37 +19,56 @@
                     var $el = $(this),
                          el = this,
                          settings = $.fn.dropit.settings;
-                    
-                    // Hide initial submenus     
+
+                    // Hide initial submenus
                     $el.addClass('dropit')
                     .find('>'+ settings.triggerParentEl +':has('+ settings.submenuEl +')').addClass('dropit-trigger')
                     .find(settings.submenuEl).addClass('dropit-submenu').hide();
-                    
+
                     // Open on click
-                    $el.on(settings.action, settings.triggerParentEl +':has('+ settings.submenuEl +') > '+ settings.triggerEl +'', function(e){
-                        e.preventDefault(); //This to prevent page jumping
-                        if($(this).parents(settings.triggerParentEl).hasClass('dropit-open')) return false;
+                    $el.on(settings.action, settings.triggerParentEl +':has('+ settings.submenuEl +') > '+ settings.triggerEl +'', function(){
+                        // Close click menu's if clicked again
+                        if(settings.action == 'click' && $(this).parents(settings.triggerParentEl).hasClass('dropit-open')){
+                            settings.beforeHide.call(this);
+                            $(this).parents(settings.triggerParentEl).removeClass('dropit-open').find(settings.submenuEl).hide();
+                            settings.afterHide.call(this);
+                            return false;
+                        }
+
+                        // Hide open menus
                         settings.beforeHide.call(this);
                         $('.dropit-open').removeClass('dropit-open').find('.dropit-submenu').hide();
                         settings.afterHide.call(this);
+
+                        // Open this menu
                         settings.beforeShow.call(this);
                         $(this).parents(settings.triggerParentEl).addClass('dropit-open').find(settings.submenuEl).show();
                         settings.afterShow.call(this);
+
                         return false;
                     });
-                    
+
                     // Close if outside click
                     $(document).on('click', function(){
                         settings.beforeHide.call(this);
                         $('.dropit-open').removeClass('dropit-open').find('.dropit-submenu').hide();
                         settings.afterHide.call(this);
                     });
-                    
+
+                    // If hover
+                    if(settings.action == 'mouseenter'){
+                        $el.on('mouseleave', function(){
+                            settings.beforeHide.call(this);
+                            $(this).removeClass('dropit-open').find(settings.submenuEl).hide();
+                            settings.afterHide.call(this);
+                        });
+                    }
+
                     settings.afterLoad.call(this);
                 });
             }
-            
-        }
+
+        };
 
         if (methods[method]) {
             return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
@@ -59,7 +78,7 @@
             $.error( 'Method "' +  method + '" does not exist in dropit plugin!');
         }
 
-    }
+    };
 
     $.fn.dropit.defaults = {
         action: 'click', // The open action for the trigger
@@ -71,8 +90,8 @@
         afterShow: function(){}, // Triggers after submenu is shown
         beforeHide: function(){}, // Triggers before submenu is hidden
         afterHide: function(){} // Triggers before submenu is hidden
-    }
+    };
 
-    $.fn.dropit.settings = {}
+    $.fn.dropit.settings = {};
 
 })(jQuery);
